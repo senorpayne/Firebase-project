0223def08a590acca04128677c5e4176b09e7976
Jason Payne <senorpayne@yahoo.com>
2018-01-22 12:59:38 -0600

    momentjs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,6 @@
 
 $('#textarea1').val('New Text');
   $('#textarea1').trigger('autoresize');
-
-
-
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyB_vvLmVGU6GEU2CZfIfz9rVdaz6USJ2bM",
@@ -14,57 +11,37 @@ $('#textarea1').val('New Text');
     messagingSenderId: "625427069198"
   };
   firebase.initializeApp(config);
-
 var dataRef = firebase.database();
 var newTrainline = "";
-
-
-
 $("#submit").on("click", function(event) {
   event.preventDefault();
  
-
 var newTrainName = $("#train-name").val().trim();
 var newDestination = $("#destination").val().trim();
 var traintime=$("#firsttime").val().trim();
 var frequency=$("#frequency").val().trim();
-
-
 var newTrainlineTemp = {
-						train:newTrainName,
-            destination:newDestination,
-            traintime:traintime,
-            frequency:frequency,
-						};
-
+                        train: newTrainName,
+            destination: newDestination,
+            traintime: traintime,
+            frequency: frequency
+                        };
 dataRef.ref().push(newTrainlineTemp);
-
-
-
-console.log(newTrainlineTemp.newTrainName);
 $("#train-name").empty()
 $("#destination").empty()
 $("#firsttime").empty()
 $("#frequency").empty()
 });
-
-
-dataRef.ref().on("child_added" , function
-	(childSnapshot) {
-console.log(childSnapshot);
-var newTrainName = childSnapshot.val().train;
-
-console.log(newTrainName);
-console.log(childSnapshot);
-console.log(moment());
-
-$("#traintable > tbody").append("<tr><td>" + newTrainName + "</td><td>" )
- // + newDestination + "</td><td>" + traintime + "</td><td>" +frequency+"</td></tr>")
-
+dataRef.ref().on("child_added", function(childSnapshot) {
+  var trainHolder = childSnapshot.val().train;
+  var destinationHolder = childSnapshot.val().destination;
+  var startTime = childSnapshot.val().traintime;
+  var frequencyHolder = childSnapshot.val().frequency;
+  var timeConverted = moment(startTime, "hh:mm").subtract(1, "years");
+  var now = moment();
+  var diffTime = moment().diff(moment(timeConverted), "minutes");
+  var tRemainder = diffTime % frequencyHolder;
+  var tMinutesTillTrain = frequencyHolder - tRemainder;
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+$("#traintable > tbody").prepend("<tr><td>" + trainHolder + "</td><td>" + destinationHolder + "</td><td>" + frequencyHolder + "</td><td>" +moment(nextTrain).format("hh:mm")+ "</td><td>"+tMinutesTillTrain+"</td></tr>")
 });
-
-
-
-
-
-
